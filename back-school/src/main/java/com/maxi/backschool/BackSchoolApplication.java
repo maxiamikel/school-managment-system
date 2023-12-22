e1bac20d59a1a637058d369c1992d09e8663a4de
maxiamikel <maxloversist@gmail.com>
2023-12-22 09:40:08 -0300

    API initialization

---
+++ back-school/src/main/java/com/maxi/backschool/BackSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.maxi.backschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSchoolApplication.class, args);
+	}
+
+}
---
+++ back-school/src/test/java/com/maxi/backschool/BackSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxi.backschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
